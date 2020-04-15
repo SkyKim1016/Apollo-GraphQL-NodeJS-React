1a275e1e7726f44a55211403271c989485fdc779
SKY <SkyKim1016@gmail.com>
2020-04-16 08:19:13 +0900

    Updated Search in React

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const server = new ApolloServer({
 
 
 mongoose
-    .connect(process.env.MONGO_URI, {useNewUrlParser:true, autoIndex:false})
+    .connect(process.env.MONGO_URI, {useNewUrlParser:true, autoIndex:true})
     .then(() => console.log('DB connected'))
     .catch( err => console.error(err));
 mongoose.set('useCreateIndex')
